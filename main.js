f3a667289b5470d59102c411f8d420364bae737c
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-20 11:09:18 -0700

    Add the response to the DOM

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
   console.log("READY, PLAYER ONE?");
+  var spotifyResponse = '';
   
   var settings = {
-  "url": "https://api.spotify.com/v1/artists/2O8wUq3dZLFKCwylQnLo0g",
+  "url": "https://api.spotify.com/v1/artists/2O8wUq3dZLFKCwylQnLo0g/albums",
   "method": "GET",
   }
   
   $.ajax(settings).done(function (response) {
     console.log(response);
-  });
+    //Append results to DOM
+    $('#target').html(JSON.stringify(response));
+  }); 
+  
 });
